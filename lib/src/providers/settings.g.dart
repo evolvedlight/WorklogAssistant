d93e436c1b9fbceee8675b8afeedbc8c1b34c592
Stephen Brown <steve@brown.bg>
2024-09-27 15:54:46 +0200

    Update classes

---
+++ lib/src/providers/settings.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'settings.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$currentThemeModeHash() => r'69e9467b1214d93df7c80f913ef0652416c93db2';
+
+/// The current theme mode of the app.
+///
+/// When this provider is first read, it will read the saved value from storage,
+/// and defaults to [ThemeMode.system] if the theme mode has not been set before.
+///
+/// Copied from [CurrentThemeMode].
+@ProviderFor(CurrentThemeMode)
+final currentThemeModeProvider =
+    AutoDisposeNotifierProvider<CurrentThemeMode, ThemeMode>.internal(
+  CurrentThemeMode.new,
+  name: r'currentThemeModeProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$currentThemeModeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CurrentThemeMode = AutoDisposeNotifier<ThemeMode>;
+String _$currentJiraFilterHash() => r'ff5cc8c6a34228678d2fff9272432a92a5382b40';
+
+/// See also [CurrentJiraFilter].
+@ProviderFor(CurrentJiraFilter)
+final currentJiraFilterProvider =
+    AutoDisposeNotifierProvider<CurrentJiraFilter, JiraFilter?>.internal(
+  CurrentJiraFilter.new,
+  name: r'currentJiraFilterProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$currentJiraFilterHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CurrentJiraFilter = AutoDisposeNotifier<JiraFilter?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
---
+++ lib/src/storage/prefs.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'prefs.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$prefsHash() => r'b9fff8caaf6065bb618e1ab5c6a00086ea2a1623';
+
+/// See also [prefs].
+@ProviderFor(prefs)
+final prefsProvider =
+    AutoDisposeFutureProvider<SharedPreferencesWithCache>.internal(
+  prefs,
+  name: r'prefsProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$prefsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef PrefsRef = AutoDisposeFutureProviderRef<SharedPreferencesWithCache>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
